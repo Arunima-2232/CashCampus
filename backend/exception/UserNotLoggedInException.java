34cc7ceb1b64f26fe4ca0e96ba6299ccbb6bddd9
Arunima-2232 <145280505+Arunima-2232@users.noreply.github.com>
2026-06-22 23:56:40 +0530

    Delete backend/exception/UserNotLoggedInException.java

--- backend/exception/UserNotLoggedInException.java
+++
@@ -1,7 +0,0 @@
-package com.expanse.expanse.manager.exception;
-
-public class UserNotLoggedInException extends RuntimeException {
-    public UserNotLoggedInException(String message) {
-        super(message);
-    }
-}
